f94c6c6c9b8ec4ad22a55afb479f9a232065c7ec
Bayrem Riahi <riahibayrem5@gmail.com>
2025-07-31 20:04:25 -0400

    Remove problematic import from main.py to fix ModuleNotFoundError.

--- src/main.py
+++ src/main.py
@@ -6,12 +6,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return jsonify(message='Welcome to FluentPass Backend!')
+    return jsonify(message="Welcome to FluentPass Backend!")
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
